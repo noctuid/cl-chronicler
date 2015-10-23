af131f1b488a5fb709ef5325eafd1ed474ebc9d4
noctuid <noct@openmailbox.org>
2015-10-22 20:45:43 -0400

    Have the total word count start at 0 for new files

--- counts.lisp
+++ counts.lisp
@@ -66,10 +66,19 @@ ACCESSOR."
 ;;; Getting and Persisting Information
 (defun update-today-info (file)
   "Store and return org heading information obtained from FILE."
-  (let ((info (parse-org-file file))
-        (store-dir (get-and-create-store-dir file))
-        (today (get-date-stamp 0)))
-    (store info (concatenate 'string store-dir today))))
+  (let* ((info (parse-org-file file))
+         (store-dir (get-and-create-store-dir file))
+         (today-file  (concatenate 'string store-dir (get-date-stamp 0)))
+         (yesterday-file (concatenate 'string store-dir (get-date-stamp 1))))
+    (unless (or (probe-file today-file)
+                (probe-file yesterday-file)
+                ;; TODO: use function that stops if even one file is found
+                ;; if this could potentially take a significant amount of time
+                (directory-files store-dir))
+      ;; the first time a file is analyzed, put initial counts in a file
+      ;; for yesterday as well so that total counts start at 0 for current day
+      (store info yesterday-file))
+    (store info today-file)))
 
 (defun get-today-info (file &optional force-update)
   "Return org heading information obtained from the stored data for FILE.
